541949f658031855f601b18adcd8f212b4197a02
Avimitin <avimitin@gmail.com>
2023-01-24 22:56:51 +0800

    Clean up library
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/endpoint/translate.rs
+++ src/endpoint/translate.rs
@@ -145,3 +145,65 @@ impl<'a> TranslateRequester<'a> {
         Ok(response)
     }
 }
+
+#[tokio::test]
+async fn test_translate_text() {
+    let key = std::env::var("DEEPL_API_KEY").unwrap();
+    let api = DeepLApi::new(&key, false);
+    let response = api.translate_text("Hello World", Lang::ZH).await.unwrap();
+
+    assert!(!response.translations.is_empty());
+
+    let translated_results = response.translations;
+    assert_eq!(translated_results[0].text, "你好，世界");
+    assert_eq!(translated_results[0].detected_source_language, Lang::EN);
+}
+
+#[tokio::test]
+async fn test_advanced_translate() {
+    let key = std::env::var("DEEPL_API_KEY").unwrap();
+    let api = DeepLApi::new(&key, false);
+
+    let response = api.translate_text(
+            "Hello World <keep additionalarg=\"test0\">This will stay exactly the way it was</keep>",
+            Lang::DE
+        )
+        .source_lang(Lang::EN)
+        .ignore_tags(vec!["keep".to_string()])
+        .tag_handling(TagHandling::Xml)
+        .await
+        .unwrap();
+
+    assert!(!response.translations.is_empty());
+
+    let translated_results = response.translations;
+    assert_eq!(
+        translated_results[0].text,
+        "Hallo Welt <keep additionalarg=\"test0\">This will stay exactly the way it was</keep>"
+    );
+    assert_eq!(translated_results[0].detected_source_language, Lang::EN);
+}
+
+#[tokio::test]
+async fn test_advanced_translator_html() {
+    let key = std::env::var("DEEPL_API_KEY").unwrap();
+    let api = DeepLApi::new(&key, false);
+
+    let response = api
+        .translate_text(
+            "Hello World <keep translate=\"no\">This will stay exactly the way it was</keep>",
+            Lang::DE,
+        )
+            .tag_handling(TagHandling::Html)
+        .await
+        .unwrap();
+
+    assert!(!response.translations.is_empty());
+
+    let translated_results = response.translations;
+    assert_eq!(
+        translated_results[0].text,
+        "Hallo Welt <keep translate=\"no\">This will stay exactly the way it was</keep>"
+    );
+    assert_eq!(translated_results[0].detected_source_language, Lang::EN);
+}
