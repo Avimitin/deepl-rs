34ebf917285e4cf72491e6f99245dc7295a4f518
Avimitin <dev@avimit.in>
2023-06-23 13:30:25 +0800

    Fix flake formatting

--- flake.nix
+++ flake.nix
@@ -9,24 +9,19 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, rust-overlay }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
-          };
-        in
-        with pkgs;
-       {
-         devShells.default = mkShell {
-           buildInputs = [
-             openssl
-             pkg-config
-             # Including cargo,clippy,cargo-fmt
-             rust-bin.stable.latest.default
-             rust-analyzer-unwrapped
-           ];
-         };
-       });
- }
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in with pkgs; {
+        devShells.default = mkShell {
+          buildInputs = [
+            openssl
+            pkg-config
+            # Including cargo,clippy,cargo-fmt
+            rust-bin.stable.latest.default
+            rust-analyzer-unwrapped
+          ];
+        };
+      });
+}
