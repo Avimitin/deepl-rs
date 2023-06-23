066df057b77532a3063c3786f97e36a2b16f1e2c
Avimitin <dev@avimit.in>
2023-06-23 13:23:05 +0800

    Add flake.nix to easily setup shell

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Dev shell for setup build environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+        in
+        with pkgs;
+       {
+         devShells.default = mkShell {
+           buildInputs = [
+             openssl
+             pkg-config
+             # Including cargo,clippy,cargo-fmt
+             rust-bin.stable.latest.default
+             rust-analyzer-unwrapped
+           ];
+         };
+       });
+ }
