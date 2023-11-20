dc988f3d37678417a599894b77ba8407a06c44d1
Avimitin <dev@avimit.in>
2023-11-20 08:34:35 +0800

    test: shorten the text in test to avoid sematic change from upstream
    Signed-off-by: Avimitin <dev@avimit.in>

--- src/endpoint/document.rs
+++ src/endpoint/document.rs
@@ -327,15 +327,12 @@ async fn test_upload_document() {
     let key = std::env::var("DEEPL_API_KEY").unwrap();
     let api = DeepLApi::with(&key).new();
 
-    let raw_text = "Doubt thou the stars are fire. \
-    Doubt that the sun doth move. \
-    Doubt truth to be a liar. \
-    But never doubt my love.";
+    let raw_text = "Hello World";
 
     tokio::fs::write("./test.txt", &raw_text).await.unwrap();
 
     let test_file = PathBuf::from("./test.txt");
-    let response = api.upload_document(&test_file, Lang::ZH).await.unwrap();
+    let response = api.upload_document(&test_file, Lang::DE).await.unwrap();
     let mut status = api.check_document_status(&response).await.unwrap();
 
     // wait for translation
@@ -358,7 +355,7 @@ async fn test_upload_document() {
         .unwrap();
 
     let content = tokio::fs::read_to_string(path).await.unwrap();
-    let expect = "怀疑你的星星是火。怀疑太阳在动。怀疑真理是个骗子。但永远不要怀疑我的爱。";
+    let expect = "Hallo Welt";
     assert_eq!(content, expect);
 }
 
