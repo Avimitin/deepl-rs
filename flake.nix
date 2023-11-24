35989ed2bb853b5a99fd798b68943a9596748da6
Avimitin <dev@avimit.in>
2023-11-24 16:48:37 +0800

    nix: fix macro not expanded issue
    Signed-off-by: Avimitin <dev@avimit.in>

--- flake.nix
+++ flake.nix
@@ -1,10 +1,14 @@
 {
-  description = "Dev shell for setup build environment";
+  description = "Flakes for Rust development";
 
   inputs = {
+    # The nixpkgs
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+    # Utility functions
     flake-utils.url = "github:numtide/flake-utils";
+
+    # An nixpkgs overlay for overriding the nixpkgs to get declarative Rust toolchain specification.
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
@@ -13,15 +17,22 @@
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
-      in with pkgs; {
-        devShells.default = mkShell {
+
+        rs-toolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
           buildInputs = [
-            openssl
-            pkg-config
-            # Including cargo,clippy,cargo-fmt
-            rust-bin.stable.latest.default
-            rust-analyzer-unwrapped
+            # Including latest cargo,clippy,cargo-fmt
+            rs-toolchain
+            # rust-analyzer comes from nixpkgs toolchain, I want the unwrapped version
+            pkgs.rust-analyzer-unwrapped
           ];
+
+          # To make rust-analyzer work correctly (The path prefix issue)
+          RUST_SRC_PATH = "${rs-toolchain}/lib/rustlib/src/rust/library";
         };
       });
 }
